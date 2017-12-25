bbcefd5aafe4f55f6da6b2b111c1d030e25f580b
nibesh <nibeshbiswas07@gmail.com>
2017-12-25 12:20:51 +0600

    first commit

---
+++ app/src/main/java/com/example/nibesh/searchviewdemo/AnimalNames.java
@@ -0,0 +1,18 @@
+package com.example.nibesh.searchviewdemo;
+
+/**
+ * Created by nibesh on 11/25/17.
+ */
+
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+}
